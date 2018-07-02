460a30410d5d56d27b59e0e9293d9f28d3718f45
Dagestan05 <steklofan@gmail.com>
2018-07-02 14:24:06 +0200

    Added bodyParser % POST route &db create

--- index.js
+++ index.js
@@ -1,8 +1,17 @@
 var express = require('express'),
+    bodyParser = require("body-parser"),
     app = express(),
     port = process.env.PORT || 3000;
 
+
 var todoRoutes = require("./routes/todos")
+
+// bodyParser setup
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}))
+
+
+
 app.get("/", function(req, res){
   res.json({message: "Hej from Express"})
 })
--- routes/todos.js
+++ routes/todos.js
@@ -3,6 +3,7 @@ var router = express.Router();
 
 var db = require("../models")
 
+// GET route
 router.get("/", function(req, res){
   db.Todo.find()
   .then(function(todos){
@@ -12,5 +13,17 @@ router.get("/", function(req, res){
     res.send(err);
   })
 })
+
+// POST route
+router.post('/', function(req, res){
+  db.Todo.create(req.body) // req.body -> object coming from bodyParser
+  .then(function(newTodo){
+    res.status(201).json(newTodo); //  status201 == created
+  })
+  .catch(function(err){
+    res.send(err);
+  })
+});
+
 
 module.exports = router;
