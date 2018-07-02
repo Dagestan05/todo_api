cfc61bd150997bde1ef73b3a490ccd10e120217e
Dagestan05 <steklofan@gmail.com>
2018-07-02 04:42:29 +0200

    added first route

--- index.js
+++ index.js
@@ -2,11 +2,20 @@ var express = require('express'),
     app = express(),
     port = process.env.PORT || 3000;
 
-
+var todoRoutes = require("./routes/todos")
 app.get("/", function(req, res){
   res.json({message: "Hej from Express"})
 })
+//use api routes
+app.use("/api/todos", todoRoutes);
 
+
+
+
+
+
+
+//server
 app.listen(port, function(){
   console.log('Server running on port ' + port)
 })
