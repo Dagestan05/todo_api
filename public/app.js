07dffaf5353affd6eb1f3052232874a5b29765e6
Dagestan05 <steklofan@gmail.com>
2018-07-03 04:06:47 +0200

    added ajax to delete

--- public/app.js
+++ public/app.js
@@ -7,7 +7,10 @@ $(document).ready(function(){
       createTodo();
       return false;    //<---- Add this line
     }})
-
+  
+  $('.list').on('click', 'span', function(){
+    removeTodo($(this).parent())
+  })
 })
 
 function addTodos(todos) {
@@ -18,22 +21,40 @@ function addTodos(todos) {
 }
 
 function addTodo(todo){
-  var newTodo = $("<li class='task'>" + todo.name +  "</li>");
-    // newTodo.completed ? newTodo.addClass('done') : newTodo;
-    if (todo.completed) {
-      newTodo.addClass('done');
-    }
-    $('.list').append(newTodo);
+  var newTodo = $("<li class='task'>" + todo.name + "<span>X</span></li>");
+  newTodo.data('id', todo._id) // .data is special jq method to store var's info for later use
+  
+  if (todo.completed) {
+    newTodo.addClass('done');
+  }
+  $('.list').append(newTodo);
 }
 function createTodo() {
   // send request to create new todo
   var userInput = $('#todoInput').val();
   $.post('/api/todos', {name: userInput})
   .then(function(newTodo){
-    $('#todoInput').val('');
+    $('#todoInput').val(' ');
     addTodo(newTodo);
   })
   .catch(function(err){
     console.log(err)
   })
+}
+
+function removeTodo(todo) {
+  var clickedId = todo.data('id');
+    var deleteUrl = '/api/todos/' + clickedId;
+    
+    $.ajax({
+      method: 'DELETE',
+      url: deleteUrl
+    })
+    .then(function(data){
+      // removing from ui
+      todo.remove();
+    })
+    .cath(function(err){
+      console.log(err)
+    })
 }
