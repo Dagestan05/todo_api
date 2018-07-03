fef7160d298457182ce6f1225d2c0bb94433ca25
Dagestan05 <steklofan@gmail.com>
2018-07-03 03:38:15 +0200

    added ajax to create & post

--- public/app.js
+++ public/app.js
@@ -1,12 +1,39 @@
 $(document).ready(function(){
   $.getJSON('/api/todos')
   .then(addTodos)
+
+  $('#todoInput').keypress(function(e) {
+    if (e.which == 13) {
+      createTodo();
+      return false;    //<---- Add this line
+    }})
+
 })
 
 function addTodos(todos) {
   //add todos to the page
   todos.forEach(function(todo){
-    var newTodo = $("<li class='task'>" + todo.name + "</li>");
-    $('.list').append(newTodo);
+    addTodo(todo)
   });
+}
+
+function addTodo(todo){
+  var newTodo = $("<li class='task'>" + todo.name +  "</li>");
+    // newTodo.completed ? newTodo.addClass('done') : newTodo;
+    if (todo.completed) {
+      newTodo.addClass('done');
+    }
+    $('.list').append(newTodo);
+}
+function createTodo() {
+  // send request to create new todo
+  var userInput = $('#todoInput').val();
+  $.post('/api/todos', {name: userInput})
+  .then(function(newTodo){
+    $('#todoInput').val('');
+    addTodo(newTodo);
+  })
+  .catch(function(err){
+    console.log(err)
+  })
 }
